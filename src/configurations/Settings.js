5f4855489d44fd82d395ce63d95e6477f07d8a7e
Satrio <c.satrio@staff.gramedia.com>
2019-04-22 01:26:21 +0700

    simplify state management logging using mobx-react-devtools

--- src/configurations/Settings.js
+++ src/configurations/Settings.js
@@ -1,6 +1,5 @@
 class Settings{
     DEBUG = true
-    DEBUG_STORE = true
     itemPerPage = 5
 }
 
--- src/store/RouterStore.js
+++ src/store/RouterStore.js
@@ -1,32 +1,17 @@
 import {observable, action, decorate} from 'mobx';
 import GlobalStore from './GlobalStore'
-import {GetCaller} from "../helpers/tracer";
-import settings from '../configurations'
 
 class RouterStore {
     getGlobal = (key) => {
-        if (settings.DEBUG_STORE) {
-            console.log(`${GetCaller()}: GET PROPS {${key}:${GlobalStore.globals[key]}}`)
-        }
         return GlobalStore.globals[key]
     }
 
     removeGlobal = (key) => {
-        if (settings.DEBUG_STORE) {
-            console.log(`${GetCaller()}: DELETE {${key}:${GlobalStore.globals[key]}}`)
-        }
         delete GlobalStore.globals[key]
     }
 
     setGlobal = (obj) => {
-        const actionString = settings.DEBUG_STORE ? GetCaller() : ''
-
         Object.keys(obj).forEach((key) => {
-            if (settings.DEBUG_STORE) {
-                const value = obj[key]
-                GlobalStore.globals[key] = value
-                console.log(`${actionString}: SET GLOBAL {${key}:${obj[key]}}`)
-            }
             GlobalStore.globals[key] = obj[key]
         });
     }
@@ -36,9 +21,9 @@ class RouterStore {
 
 decorate(RouterStore, {
     store: observable,
-    // setGlobal: action,
-    // getGlobal: action,
-    // removeGlobal: action
+    setGlobal: action,
+    getGlobal: action,
+    removeGlobal: action
 })
 
 export default RouterStore;
