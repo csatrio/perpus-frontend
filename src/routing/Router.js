01fc56f9d980b14771d187ab140e8bcf803d834f
Satrio <c.satrio@staff.gramedia.com>
2019-04-24 02:10:36 +0700

    remove RouteWrapper indirection to increase rendering speed

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -5,15 +5,17 @@ import Anggota from '../pages/Anggota'
 import Buku from '../pages/Buku'
 import Sewa from '../pages/sewa/Sewa'
 import InputForm from '../components/InputForm'
-import RouteWrapper from './RouteWrapper'
+import {observer, inject} from 'mobx-react'
+
+const doInject = (component) => inject('store', 'settings')(observer(component))
 
 const RoutePath =
     () => (
         <Switch>
-            <RouteWrapper path='/' exact component={Sample}/>
-            <RouteWrapper path='/anggota' component={Anggota}/>
-            <RouteWrapper path='/buku' component={Buku}/>
-            <RouteWrapper path='/sewa' component={Sewa}/>
+            <Route path='/' exact component={doInject(Sample)}/>
+            <Route path='/anggota' component={doInject(Anggota)}/>
+            <Route path='/buku' component={doInject(Buku)}/>
+            <Route path='/sewa' component={doInject(Sewa)}/>
             <Route path='/input' component={InputForm}/>
         </Switch>
     );
